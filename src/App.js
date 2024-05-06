c99333802b54c26098697782ca1dd92723bcb94b
nightburnn <nightburn2003@gmail.com>
2024-05-06 20:42:00 +0100

    fixed git conflict

--- src/App.js
+++ src/App.js
@@ -16,19 +16,3 @@ function App() {
 }
 
 export default App;
-
-import Statistics from './components/Statistics/Statistics'
-import Header from "./components/Header/Header";
-import { BrowserRouter as Router } from 'react-router-dom';
-
-
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Statistics/>
-    </Router>
-  );
-}
-
-export default App;
